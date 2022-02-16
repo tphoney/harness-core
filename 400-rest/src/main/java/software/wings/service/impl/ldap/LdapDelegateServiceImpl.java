47485d1bd59dd06044aac153c96e1806418f5c0d
Ujjawal <ujjawal.prasad@harness.io>
2022-02-16 15:26:11 +0530

    [PL-22404]: Added ldap changes

--- 400-rest/src/main/java/software/wings/service/impl/ldap/LdapDelegateServiceImpl.java
+++ 400-rest/src/main/java/software/wings/service/impl/ldap/LdapDelegateServiceImpl.java
@@ -149,7 +149,9 @@ public class LdapDelegateServiceImpl implements LdapDelegateService {
     String externalUserId = "";
     if (user.getAttributes() != null && Arrays.asList(user.getAttributeNames()).contains(userConfig.getUidAttr())
         && user.getAttribute(userConfig.getUidAttr()) != null) {
-      externalUserId = user.getAttribute(userConfig.getUidAttr()).getStringValue().toLowerCase();
+      if (StringUtils.isNotEmpty(user.getAttribute(userConfig.getSamAccountNameAttr()).getStringValue())) {
+        externalUserId = user.getAttribute(userConfig.getUidAttr()).getStringValue().toLowerCase();
+      }
     }
 
     if (user.getAttributes() != null
@@ -157,7 +159,9 @@ public class LdapDelegateServiceImpl implements LdapDelegateService {
         && user.getAttribute(userConfig.getSamAccountNameAttr()) != null) {
       log.info("LDAP user with email {} samAccountName set as {}", email,
           user.getAttribute(userConfig.getSamAccountNameAttr()).getStringValue());
-      externalUserId = user.getAttribute(userConfig.getSamAccountNameAttr()).getStringValue().toLowerCase();
+      if (StringUtils.isNotEmpty(user.getAttribute(userConfig.getSamAccountNameAttr()).getStringValue())) {
+        externalUserId = user.getAttribute(userConfig.getSamAccountNameAttr()).getStringValue().toLowerCase();
+      }
     }
 
     log.info("LDAP user response with name {} and email {} and userId {}", name, email, externalUserId);

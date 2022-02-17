35a0e15a129de8ae7ea03801e02fec7dc65e6529
Prashant Pal <prashant.pal@harness.io>
2022-02-17 19:09:34 +0530

    fix: [PIE-0]: remove versioned

--- 910-delegate-service-driver/src/main/java/io/harness/grpc/DelegateServiceDriverGrpcClientModule.java
+++ 910-delegate-service-driver/src/main/java/io/harness/grpc/DelegateServiceDriverGrpcClientModule.java
@@ -81,7 +81,7 @@ public class DelegateServiceDriverGrpcClientModule extends ProviderModule {
       authorityToUse = defaultAuthority;
     } else if (!("ONPREM".equals(deployMode) || "KUBERNETES_ONPREM".equals(deployMode))) {
       String versionPrefix = "v-" + versionInfo.getVersion().replace('.', '-') + "-";
-      String versionedAuthority = versionPrefix + authority;
+      String versionedAuthority = authority;
       if (isValidAuthority(versionedAuthority)) {
         log.info("Using versioned authority: {}", versionedAuthority);
         authorityToUse = versionedAuthority;

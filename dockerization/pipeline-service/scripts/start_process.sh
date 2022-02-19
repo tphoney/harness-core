12f771a752182495bc1f9bfd2e691d8ce6d2cb69
Sanyasi Naidu Annepu <sanyasi.naidu@harness.io>
2022-02-19 15:18:11 +0530

    [PIE-0]: Add tools path to start.sh

--- dockerization/pipeline-service/scripts/start_process.sh
+++ dockerization/pipeline-service/scripts/start_process.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Copyright 2021 Harness Inc. All rights reserved.
-# Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+# Use of this source code is governed by the PolyForm Shield 1.0.0 license
 # that can be found in the licenses directory at the root of this repository, also available at
-# https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+# https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
 
 set -x
 if [[ -v "{hostname}" ]]; then
@@ -29,6 +29,10 @@ else
     export GC_PARAMS=" -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=40 -XX:MaxGCPauseMillis=1000 -Dfile.encoding=UTF-8"
 fi
 
+if [[ -z "$EXPERIMENTAL_GC" ]]; then
+    GC_PARAMS=$EXPERIMENTAL_GC
+fi
+
 export JAVA_OPTS="-Xmx${MEMORY} -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:mygclogfilename.gc $GC_PARAMS $JAVA_ADVANCED_FLAGS"
 
 if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
@@ -40,6 +44,10 @@ if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
 fi
 
 JAVA_OPTS=$JAVA_OPTS" -Xbootclasspath/p:/opt/harness/alpn-boot-8.1.13.v20181017.jar"
+
+if [[ "${ENABLE_TOOLS}" == "true" ]]; then
+   JAVA_OPTS=$JAVA_OPTS" -Xbootclasspath/a:/usr/lib/jvm/java-1.8-openjdk/lib/tools.jar"
+fi
 
 if [[ "${DEPLOY_MODE}" == "KUBERNETES" || "${DEPLOY_MODE}" == "KUBERNETES_ONPREM" || "${DEPLOY_VERSION}" == "COMMUNITY" ]]; then
     java $JAVA_OPTS -jar $CAPSULE_JAR $COMMAND /opt/harness/config.yml

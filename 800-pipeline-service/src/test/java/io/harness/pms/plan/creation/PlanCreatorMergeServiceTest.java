11cf5d98560ad1d4576bc62b845fe7350a56376c
Prashant Sharma <prashant.sharma@harness.io>
2022-02-21 17:27:45 +0530

    added test for create plan

--- 800-pipeline-service/src/test/java/io/harness/pms/plan/creation/PlanCreatorMergeServiceTest.java
+++ 800-pipeline-service/src/test/java/io/harness/pms/plan/creation/PlanCreatorMergeServiceTest.java
@@ -11,31 +11,66 @@ import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
 import static io.harness.rule.OwnerRule.NAMAN;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
 
 import io.harness.CategoryTest;
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.category.element.UnitTests;
+import io.harness.execution.PlanExecutionMetadata;
 import io.harness.pms.contracts.plan.ExecutionMetadata;
 import io.harness.pms.contracts.plan.PlanCreationContextValue;
 import io.harness.pms.contracts.triggers.ParsedPayload;
 import io.harness.pms.contracts.triggers.SourceType;
 import io.harness.pms.contracts.triggers.TriggerPayload;
+import io.harness.pms.plan.creation.validator.PlanCreationValidator;
+import io.harness.pms.sdk.PmsSdkHelper;
+import io.harness.pms.yaml.YamlUtils;
 import io.harness.rule.Owner;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.concurrent.Executors;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 @OwnedBy(PIPELINE)
 public class PlanCreatorMergeServiceTest extends CategoryTest {
+  @Mock PmsSdkHelper pmsSdkHelper;
+  @Mock
+  PlanCreationValidator planCreationValidator;
+  @InjectMocks PlanCreatorMergeService planCreatorMergeService;
+
+  String accountId = "acc";
+  String orgId = "org";
+  String projId = "proj";
+
+  private String getYamlFieldFromGivenFileName(String file) throws IOException {
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    InputStream yamlFile = classLoader.getResourceAsStream(file);
+    assertThat(yamlFile).isNotNull();
+
+    String yaml = new Scanner(yamlFile, "UTF-8").useDelimiter("\\A").next();
+    yaml = YamlUtils.injectUuid(yaml);
+    return yaml;
+  }
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+  }
+
   @Test
   @Owner(developers = NAMAN)
   @Category(UnitTests.class)
   public void testCreateInitialPlanCreationContext() {
-    String accountId = "acc";
-    String orgId = "org";
-    String projId = "proj";
     ExecutionMetadata executionMetadata = ExecutionMetadata.newBuilder()
                                               .setExecutionUuid("execId")
                                               .setRunSequence(3)
@@ -69,5 +104,25 @@ public class PlanCreatorMergeServiceTest extends CategoryTest {
     assertThat(planCreationContextValue.getProjectIdentifier()).isEqualTo(projId);
     assertThat(planCreationContextValue.getMetadata()).isEqualTo(executionMetadata);
     assertThat(planCreationContextValue.getTriggerPayload()).isEqualTo(triggerPayload);
+  }
+
+  @Test
+  @Owner(developers = NAMAN)
+  @Category(UnitTests.class)
+  public void testCreatePlan() throws IOException {
+    ExecutionMetadata executionMetadata = ExecutionMetadata.newBuilder()
+            .setExecutionUuid("execId")
+            .setRunSequence(3)
+            .setModuleType("cd")
+            .setPipelineIdentifier("pipelineId")
+            .build();
+
+    TriggerPayload triggerPayload = TriggerPayload.newBuilder()
+            .setParsedPayload(ParsedPayload.newBuilder().build())
+            .setSourceType(SourceType.GITHUB_REPO)
+            .build();
+    PlanExecutionMetadata planExecutionMetadata = PlanExecutionMetadata.builder().processedYaml(getYamlFieldFromGivenFileName("pipeline.yml")).triggerPayload(triggerPayload).build();
+    planCreatorMergeService.createPlan(accountId,orgId,projId,executionMetadata,planExecutionMetadata);
+    verify(planCreationValidator).validate(any(),any());
   }
 }

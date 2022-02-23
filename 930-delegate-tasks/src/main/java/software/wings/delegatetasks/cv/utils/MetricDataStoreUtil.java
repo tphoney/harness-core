65ed18efdf4dbdd80632c22aa682d74be8a92a7f
Abhijith Pradeep <abhijith.pradeep@harness.io>
2022-02-23 16:16:44 +0530

    [CVS-9175]: StateType moved out

---
+++ 930-delegate-tasks/src/main/java/software/wings/delegatetasks/cv/utils/MetricDataStoreUtil.java
@@ -0,0 +1,33 @@
+package software.wings.delegatetasks.cv.utils;
+
+import com.google.common.util.concurrent.TimeLimiter;
+import com.google.inject.Inject;
+import io.harness.concurrent.HTimeLimiter;
+import io.harness.managerclient.VerificationServiceClient;
+import io.harness.rest.RestResponse;
+import software.wings.delegatetasks.cv.beans.NewRelicMetricDataRecord;
+
+import java.time.Duration;
+import java.util.List;
+
+import static io.harness.network.SafeHttpCall.execute;
+
+public class MetricDataStoreUtil {
+
+    @Inject
+    private VerificationServiceClient verificationClient;
+    @Inject private TimeLimiter timeLimiter;
+
+    public boolean saveMetricData(String accountId, String applicationId, String stateExecutionId,
+                                  String delegateTaskId, List<NewRelicMetricDataRecord> metricData) throws Exception {
+        if (metricData.isEmpty()) {
+            return true;
+        }
+
+        RestResponse<Boolean> restResponse = HTimeLimiter.callInterruptible21(timeLimiter, Duration.ofSeconds(15),
+                ()
+                        -> execute(verificationClient.saveTimeSeriesMetrics(
+                        accountId, applicationId, stateExecutionId, delegateTaskId, metricData)));
+        return restResponse.getResource();
+    }
+}

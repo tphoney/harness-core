1e47ddd22feec0166221d18b66582c914e5be7c5
Anjan Balgovind <anjan.balgovind@harness.io>
2022-02-23 13:56:39 -0800

    fix: [CVS-9098]: fixing checkstyle

--- 300-cv-nextgen/src/main/java/io/harness/cvng/core/beans/monitoredService/healthSouceSpec/CustomHealthSourceSpec.java
+++ 300-cv-nextgen/src/main/java/io/harness/cvng/core/beans/monitoredService/healthSouceSpec/CustomHealthSourceSpec.java
@@ -32,7 +32,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;

657375279a61f24d53acffe4f277bf6b9eb55bba
VikasMaddukuriHarness <vikas.maddukuri@harness.io>
2022-02-24 15:50:40 +0530

    fix: [Pl-23146]: GraphQl API fix  (#30975)
    * [PL-23146]: HotFix for GraphqlAPI
    
    * fix: [PL-21394]: remove attributes from ldap query
    
    Co-authored-by: Ujjawal <ujjawal.prasad@harness.io>

--- 400-rest/src/main/java/software/wings/service/impl/ldap/LdapDelegateServiceImpl.java
+++ 400-rest/src/main/java/software/wings/service/impl/ldap/LdapDelegateServiceImpl.java
@@ -8,6 +8,7 @@
 package software.wings.service.impl.ldap;
 
 import static io.harness.annotations.dev.HarnessTeam.PL;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 
 import io.harness.annotations.dev.OwnedBy;
 import io.harness.security.encryption.EncryptedDataDetail;
@@ -147,7 +148,9 @@ public class LdapDelegateServiceImpl implements LdapDelegateService {
     }
 
     String externalUserId = "";
-    if (user.getAttributes() != null && Arrays.asList(user.getAttributeNames()).contains(userConfig.getUidAttr())
+    log.info("LDAPIterator: user with email {} ldap entry is {}", email, user);
+    if (isNotEmpty(user.getAttributeNames())
+        && Arrays.asList(user.getAttributeNames()).contains(userConfig.getUidAttr())
         && user.getAttribute(userConfig.getUidAttr()) != null) {
       log.info("LDAP user with email {} user uid set as {}", email,
           user.getAttribute(userConfig.getUidAttr()).getStringValue());
@@ -156,7 +159,7 @@ public class LdapDelegateServiceImpl implements LdapDelegateService {
       }
     }
 
-    if (user.getAttributes() != null
+    if (isNotEmpty(user.getAttributeNames())
         && Arrays.asList(user.getAttributeNames()).contains(userConfig.getSamAccountNameAttr())
         && user.getAttribute(userConfig.getSamAccountNameAttr()) != null) {
       log.info("LDAP user with email {} samAccountName set as {}", email,
